db1cf6b79df9a68a7e039a911ed8c0f096a84065
Emre kavcakar <kavcakaremre@gmail.com>
2023-12-06 00:44:15 -0500

    card

--- rc07_nba-legends/src/components/CardContainer.jsx
+++ rc07_nba-legends/src/components/CardContainer.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import Container from "react-bootstrap/esm/Container";
+import { data } from "../helpers/data";
 
 const CardContainer = () => {
-  return <div>CardContainer</div>;
+  return;
+  <Container className="card-container rounded-4 my-4 p-3">
+    {data.map((player) => (
+      <p>{player.name}</p>
+    ))}
+  </Container>;
 };
-
 export default CardContainer;
