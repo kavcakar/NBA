c48cc05272ab7fdaa00366d2246d587188ad0ad5
Emre kavcakar <kavcakaremre@gmail.com>
2023-12-13 13:12:06 -0500

    commit

--- rc07_nba-legends/src/App.js
+++ rc07_nba-legends/src/App.js
@@ -1,7 +1,6 @@
 import Container from "react-bootstrap/Container";
 import CardContainer from "./components/CardContainer";
 import Header from "./components/Header";
-
 function App() {
   return (
     <Container className="text-center mt-4">
--- rc07_nba-legends/src/components/Header.jsx
+++ rc07_nba-legends/src/components/Header.jsx
@@ -1,14 +1,14 @@
 import Container from "react-bootstrap/Container";
 import Image from "react-bootstrap/Image";
 import nbaLogo from "../assets/nba-logo.png";
-
+import Image from "react-bootstrap/Image";
+import nbaLogo from "../assets/nba-logo.png";
 const Header = () => {
   return (
-    <Container className=" text-center mt-4">
-      <Image src={nbaLogo}></Image>
-      <h1 className="my-2 font-monospace display-4">NBA LEGENDS</h1>
+    <Container>
+      <Image src={nbaLogo} width="200px"></Image>
+      <h1 className="my-2 font-monospace display-4 fw-bold">NBA Legends</h1>
     </Container>
   );
 };
-
 export default Header;
