06f81cccb3560e098a81f906b4df8ba05788e17a
Emre kavcakar <kavcakaremre@gmail.com>
2024-01-19 12:49:52 -0500

    play

--- rc07_nba-legends/src/components/PlayerCard.jsx
+++ rc07_nba-legends/src/components/PlayerCard.jsx
@@ -4,6 +4,11 @@ const PlayerCard = ({ name, img, statistics }) => {
   return (
     <Card className="rounded-2 m-auto player-card" role="button">
       <Card.Img variant="top" src={img} className="player-logo" />
+      <ul>
+        {statistics.map((item, i) => {
+          return <li key={i}>{item}</li>;
+        })}
+      </ul>
       <Card.Footer>
         <Card.Title>{name}</Card.Title>
       </Card.Footer>
