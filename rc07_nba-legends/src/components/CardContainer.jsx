c30bb1b9d4586cb9a5c603d347bc0e9fc548d9da
Emre kavcakar <kavcakaremre@gmail.com>
2024-01-19 23:48:52 -0500

    filter

--- rc07_nba-legends/src/components/CardContainer.jsx
+++ rc07_nba-legends/src/components/CardContainer.jsx
@@ -1,23 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
+import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
 import { data } from "../helpers/data";
 import PlayerCard from "./PlayerCard";
 
 const CardContainer = () => {
+  const [search, setSearch] = useState("");
+
   console.log("Data:", data);
 
   return (
-    <Container className="card-container rounded-4 my-4 p-3">
-      <Row>
-        {data.map((player, i) => (
-          <Col md={6} lg={4} xl={3} key={i}>
-            <PlayerCard {...player} />
-          </Col>
-        ))}
-      </Row>
-    </Container>
+    <>
+      <Form.Control
+        placeholder="Search Player..."
+        type="search"
+        className="w-50  m-auto"
+        onChange={(e) => setSearch(e.target.value)}
+      />
+
+      <Container className="card-container rounded-4 my-4 p-3">
+        <Row>
+          {data
+            .filter((player) =>
+              player.name
+                .toLocaleLowerCase()
+                .includes(search.toLocaleLowerCase())
+            )
+            .map((player, i) => (
+              <Col md={6} lg={4} xl={3} key={i}>
+                <PlayerCard {...player} />
+              </Col>
+            ))}
+        </Row>
+      </Container>
+    </>
   );
 };
 
