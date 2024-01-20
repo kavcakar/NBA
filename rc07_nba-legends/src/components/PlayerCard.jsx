fa3dc991f40eb85dde26781bb7ef7924276aeaae
Emre kavcakar <kavcakaremre@gmail.com>
2024-01-19 22:44:28 -0500

    usestate

--- rc07_nba-legends/src/components/PlayerCard.jsx
+++ rc07_nba-legends/src/components/PlayerCard.jsx
@@ -1,14 +1,28 @@
+import { useState } from "react";
 import Card from "react-bootstrap/Card";
 
 const PlayerCard = ({ name, img, statistics }) => {
+  const [showImage, setShowImage] = useState(true);
+
   return (
-    <Card className="rounded-2 m-auto player-card" role="button">
-      <Card.Img variant="top" src={img} className="player-logo" />
-      <ul>
-        {statistics.map((item, i) => {
-          return <li key={i}>{item}</li>;
-        })}
-      </ul>
+    <Card
+      onClick={() => setShowImage(!showImage)}
+      className="rounded-2 m-auto player-card"
+      role="button"
+    >
+      {showImage ? (
+        <Card.Img variant="top" src={img} className="player-logo" />
+      ) : (
+        <ul className="m-auto">
+          {statistics.map((item, i) => {
+            return (
+              <li key={i} className="h5 text-start list-unstyled">
+                🏀 {item}
+              </li>
+            );
+          })}
+        </ul>
+      )}
       <Card.Footer>
         <Card.Title>{name}</Card.Title>
       </Card.Footer>
